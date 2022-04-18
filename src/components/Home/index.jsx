aafcf3649384ef1170044249a5e968ef8c50049c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-18 13:14:22 +0200

    feat: finish project

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
 import style from "./styles.module.scss";
-
+import homeBg from "../../assets/Group2.svg";
+import icon from "../../assets/icon.svg";
 export const Home = () => {
   return (
     <article className={style.section}>
       <div className={style.container}>
-        <h1>Hello world</h1>
+        <div>
+          <h1>
+            Protect Our <br /> <span>Water Buddies</span>
+          </h1>
+          <p>
+            You did not inherit the Earths water from your ancestors. You awe it
+            to your future generations ahead.
+          </p>
+          <div>
+            <button>Lets Begin</button>
+            <button>
+              <img src={icon} alt="icon" />
+              Watch Video
+            </button>
+          </div>
+        </div>
+        <div className={style.bg}>
+          <img src={homeBg} alt="" />
+        </div>
       </div>
     </article>
   );
